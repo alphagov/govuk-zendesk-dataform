4d274118f2e126b2723febfaef3569e3788d9bc0
Oliver Roberts <oliver.roberts1@digital.cabinet-office.gov.uk>
2026-01-15 10:11:38 +0000

    corrected output table for production

--- definitions/outputs/conquad_zendesk_dataform.js
+++ definitions/outputs/conquad_zendesk_dataform.js
@@ -6,7 +6,7 @@ if (currentEnv === "production") {
     type: "table",
     description: "This table is only created in production",
     database: "gds-bq-reporting",
-    schema: "zendesk_processing_publishing_exploration",
+    schema: "zendesk_processing_publishing",
     tags: ["production_only"],
     dependencies: ['conquad_output']
   })
